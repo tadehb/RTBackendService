3002443b59f430f6214c63102ed0c4fef52c0026
t.boghosian <t.boghosian@DESKTOP-FQHU6LG>
2021-02-01 10:53:15 +0330

    Adding Layers

---
+++ RTBackendService/Infrastructure/Interfaces/GlobalInterfaces.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Interfaces
+{
+
+    public interface ISingleton { }
+    public interface IScoped { }
+    public interface ITransient { }
+    public class GlobalInterfaces
+    {
+    }
+}
---
+++ RTBackendService/WebApi/Configs/DiWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebApi.Configs
+{
+    public static class DiWrapper
+    {
+
+        public static void DiWrapperConf(this IServiceCollection services, IConfiguration configuration)
+        {
+            Assembly[] assemblies = configuration.GetSection("Service:Assemblies")
+                .GetChildren()
+                .AsEnumerable()
+                .Select(x => $"{x.Value}.dll")
+                .Select(x => Directory.GetFiles(Directory.GetCurrentDirectory(), x).Single())
+                .Select(x => Assembly.LoadFrom(x))
+                .ToArray();
+
+            Parallel.ForEach(assemblies, assembly =>
+            {
+                Type[] singles = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(ISingleton))).ToArray();
+                Type[] scopes = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(IScoped))).ToArray();
+                Type[] transients = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(ITransient))).ToArray();
+
+                Parallel.ForEach(singles, single => services.AddSingleton(single));
+                Parallel.ForEach(scopes, scope => services.AddScoped(scope));
+                Parallel.ForEach(transients, transient => services.AddTransient(transient));
+            });
+        }
+    }
+}
